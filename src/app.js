254f998c25592deefc4666962cf1e13d1c41c33f
Sergio Bonaque Rubio <s.bonaquecontacto@gmail.com>
2025-10-21 13:08:12 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -10,8 +10,18 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-document.querySelector("#excuse").innerHTML = "Hello Rigo!"
+function getRandomNumber (a, b) {
+  return Math.floor (Math.random() * (b - a + 1)) + a;
+}
+
+
+function getRandom (anyArray) {
+  let max = anyArray.length -1;
+  let min = 0;
+  let random = getRandomNumber (min, max);
+  return anyArray [random];
+
+}
+
+
+document.querySelector("#excuse").innerHTML = getRandom (who) + ' ' + getRandom (action) + ' ' + getRandom (what) + ' ' + getRandom (when);
